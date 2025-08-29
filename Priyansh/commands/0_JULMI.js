154d2d23283a6e1c9079f880df2d49e4d3cd633f
arslanji0991 <arslanji0991@gmail.com>
2025-08-29 23:34:04 +0530

    Update 0_JULMI.js

--- Priyansh/commands/0_JULMI.js
+++ Priyansh/commands/0_JULMI.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+iconst fs = require("fs");
 module.exports.config = {
 	name: "Julmi",
     version: "1.0.1",
@@ -21,12 +21,5 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("Admin")) {
 		var msg = {
 				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖जुल्मी≛जाट❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://i.postimg.cc/BQXSmpbK/IMG-20250829-212925-062.jpg`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/Julmi.jpg`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
-
-					}
+				
+			
