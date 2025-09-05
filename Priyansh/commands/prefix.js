c78cf01f9bfee2f666e41f687ce38e1df8831755
motojaan5679 <motojaan5679@gmail.com>
2025-09-06 02:20:17 +0530

    Update prefix.js

--- Priyansh/commands/prefix.js
+++ Priyansh/commands/prefix.js
@@ -25,10 +25,10 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
     if (body === i.toUpperCase() | body === i | str === body) {
 const prefix = threadSetting.PREFIX || global.config.PREFIX;
       if (data.PREFIX == null) {
-        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n💝🥀༻𝐎𝐖𝐍𝐄𝐑:- ☞𝗝𝗨𝗟𝗠𝗜 𝗝𝗔𝗔𝗧☜ ༺ ༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠𝗝𝗨𝗟𝗠𝗜〠.༒ ༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL☜\n
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @julmi_jaat😇`)
+        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n💝🥀༻𝐎𝐖𝐍𝐄𝐑:- ☞𝗝𝗔𝗠𝗔𝗟 𝗣𝗔𝗧𝗛𝗔𝗡☜ ༺ ༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠𝗝𝗔𝗠𝗟𝗨〠.༒ ༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ https://www.facebook.com/share/19W2oNtUuX/☜\n
+👋For Any Kind Of Help Contact On Telegram  Username 👉 @jamal_ji😇`)
       }
-      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n💝🥀༻𝐎𝐖𝐍𝐄𝐑:- ☞𝗝𝗨𝗟𝗠𝗜 𝗝𝗔𝗔𝗧☜ ༺ ༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠𝗝𝗨𝗟𝗠𝗜〠.༒ ༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL☜\n👋For Any Kind Of Help Contact On Instagram 👇👇https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==😇' + data.PREFIX)
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n💝🥀༻𝐎𝐖𝐍𝐄𝐑:- ☞𝗝𝗔𝗠𝗔𝗟 𝗣𝗔𝗧𝗛𝗔𝗡☜ ༺ ༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠𝗝𝗔𝗠𝗟𝗨〠.༒ ༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ https://www.facebook.com/share/19W2oNtUuX/☜\n👋For Any Kind Of Help Contact On Instagram 👇👇https://www.instagram.com/alex972740?igsh=MWNpbDFnMzYyM3p5Nw==😇' + data.PREFIX)
     }
 
   });
